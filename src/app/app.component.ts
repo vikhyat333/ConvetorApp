da376161a1900401ef962ecd19c67f5400c90aa3
Viky <vikone21@gmail.com>
2020-08-27 18:00:29 +0530

    till attribute binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,34 @@ export class AppComponent {
   readme(){
     console.log('hello king');        
   }
+  show1=false;
+  show2=false;
 
-  showMe='true';
+
+  swithValue='six';
+  //showElse='true';
+
+  showMe=true;
   colorName='green';
+
+  bgColor='yellow';
+
+  users =[
+    {id:1,name: 'John1'},
+    {id:2,name: 'John2'},
+    {id:3,name: 'John3'},
+    {id:4,name: 'John4'},
+    {id:5,name: 'John5'},
+    {id:6,name: 'John6'}
+  ];
+
+  clsName='one';
+  clsName1='two';
+
+  Href1= 'http://localhost:4200';
+  subString = 'this is just a string! binding with innner html prop - one way binding'
+  updatedLink='www.google.com'
+
 
   //@HostListener()
 }
